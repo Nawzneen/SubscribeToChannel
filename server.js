ce8ce55d462df7c4c12667840ef3c0f369cce01f
nawzneen <earworm90@gmail.com>
2023-05-22 09:46:39 +0300

    add cors

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+// to solve the cross origin problem
+const cors = require("cors");
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("connected to databasse"));
+app.use(cors());
 app.use(express.json());
 const subscribersRouter = require("./routes/subscribers");
 app.use("/subscribers", subscribersRouter);
