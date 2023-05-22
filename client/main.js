173768ce403e4fe2b589d61fd0b344b7617bb2c0
nawzneen <earworm90@gmail.com>
2023-05-22 09:47:09 +0300

    minor changes

---
+++ client/main.js
@@ -0,0 +1,27 @@
+const btnElement = document.getElementById("btn");
+btnElement.addEventListener("click", (event) => {
+  console.log("button is clicked");
+});
+
+const formElement = document.getElementById("form");
+formElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const nameValue = document.getElementById("name").value;
+  const subscribedValue = document.getElementById("subscribedToChannel").value;
+
+  const formData = {
+    name: nameValue,
+    subscribedToChannel: subscribedValue,
+  };
+
+  axios
+    .post("http://localhost:3000/subscribers", formData)
+    .then((response) => {
+      console.log("success", response.data);
+      alert("Subscriber added successfully");
+    })
+    .catch((error) => {
+      console.error("error", error.response.data);
+      alert("Failed. Please try again.");
+    });
+});
